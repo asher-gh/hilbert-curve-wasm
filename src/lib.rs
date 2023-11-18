168983a1b3dea662b37f63256f7107de0c6909ec
Asher <ashish.2017@hotmail.com>
2023-11-18 11:19:28 +0000

    pure js implementation

--- src/lib.rs
+++ src/lib.rs
@@ -3,126 +3,193 @@ use fixedbitset::FixedBitSet;
 use js_sys::Math;
 use wasm_bindgen::prelude::*;
 
-const SIZE: u32 = 512;
+// const
+// const SIZE: u8 = 4;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-#[repr(u8)] // represent each cel as a single byte
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum GameCell {
-    Dead = 0, // simplifies cell's live neighbour counting with addition
-    Alive = 1,
-}
-
-#[wasm_bindgen]
+#[derive(Default)]
 pub struct Universe {
-    width: u32,
-    height: u32,
-    cells: FixedBitSet,
+    x: u32,
+    y: u32,
+    n: u32,
+    s: u32,
+    d: u32,
 }
 
 #[wasm_bindgen]
-impl Universe {
-    pub fn new() -> Self {
-        let width = SIZE;
-        let height = SIZE;
-
-        let size = (width * height) as usize;
-        let mut cells = FixedBitSet::with_capacity(size);
-
-        for i in 0..size {
-            cells.set(i, Math::random() < 0.4)
-        }
-
-        Universe {
-            width,
-            height,
-            cells,
-        }
+impl HilbertCurve {
+    pub fn new(order: u32) -> Self {
+        Self {}
     }
 
-    pub fn width(&self) -> u32 {
-        self.width
-    }
-
-    pub fn height(&self) -> u32 {
-        self.height
-    }
-
-    pub fn cells(&self) -> *const u32 {
-        self.cells.as_slice().as_ptr()
-    }
-
-    // pub fn render(&self) -> String {
-    // self.to_string()
-    // }
-
-    fn get_index(&self, row: u32, column: u32) -> usize {
-        (row * self.width + column) as usize
-    }
-
-    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        let mut count = 0;
-
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
-                }
-
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8;
-            }
-        }
-
-        count
-    }
-
-    pub fn tick(&mut self) {
-        let mut next = self.cells.clone();
-
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-                next.set(
-                    idx,
-                    match (cell, self.live_neighbor_count(row, col)) {
-                        (true, x) if x < 2 => false,
-                        (true, 2 | 3) => true,
-                        (true, x) if x > 3 => false,
-                        (false, 3) => true,
-                        (otherwise, _) => otherwise,
-                    },
-                )
-            }
-        }
-
-        self.cells = next;
-    }
+    // convert (x,y) to d
+    // pub fn xy2d()
 }
 
-// impl fmt::Display for Universe {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         for line in self.cells.as_slice().chunks(self.width as usize) {
-//             for &cell in line {
-//                 let symbol = if cell == GameCell::Dead { '◻' } else { '◼' };
-//                 write!(f, "{symbol}")?;
-//             }
-//             write!(f, "\n")?;
+// #[wasm_bindgen]
+// extern "C" {
+//     fn alert(s: &str);
+// }
+//
+// #[wasm_bindgen]
+// #[repr(u8)] // represent each cel as a single byte
+// #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+// pub enum GameCell {
+//     Dead = 0, // simplifies cell's live neighbour counting with addition
+//     Alive = 1,
+// }
+//
+// #[wasm_bindgen]
+// pub struct Universe {
+//     width: u32,
+//     height: u32,
+//     cells: FixedBitSet,
+//     x: u8,
+//     y: u8,
+// }
+//
+// #[wasm_bindgen]
+// impl Universe {
+//     pub fn new() -> Self {
+//         let width = SIZE;
+//         let height = SIZE;
+//
+//         let size = (width * height) as usize;
+//         let mut cells = FixedBitSet::with_capacity(size);
+//
+//         for i in 0..size {
+//             cells.set(i, Math::random() < 0.4)
 //         }
 //
-//         Ok(())
+//         Universe {
+//             width,
+//             height,
+//             cells,
+//             x: 0,
+//             y: 0,
+//         }
+//     }
+//
+//     pub fn width(&self) -> u32 {
+//         self.width
+//     }
+//
+//     pub fn height(&self) -> u32 {
+//         self.height
+//     }
+//
+//     pub fn cells(&self) -> *const u32 {
+//         self.cells.as_slice().as_ptr()
+//     }
+//
+//     pub fn x(&self) -> *const u8 {
+//         &self.x
+//     }
+//
+//     pub fn y(&self) -> *const u8 {
+//         &self.y
+//     }
+//
+//     // pub fn render(&self) -> String {
+//     // self.to_string()
+//     // }
+//
+//     fn get_index(&self, row: u32, column: u32) -> usize {
+//         (row * self.width + column) as usize
+//     }
+//
+//     fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+//         let mut count = 0;
+//
+//         for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+//             for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+//                 if delta_row == 0 && delta_col == 0 {
+//                     continue;
+//                 }
+//
+//                 let neighbor_row = (row + delta_row) % self.height;
+//                 let neighbor_col = (column + delta_col) % self.width;
+//                 let idx = self.get_index(neighbor_row, neighbor_col);
+//                 count += self.cells[idx] as u8;
+//             }
+//         }
+//
+//         count
+//     }
+//
+//     pub fn tick(&mut self) {
+//         let mut next = self.cells.clone();
+//
+//         for row in 0..self.height {
+//             for col in 0..self.width {
+//                 let idx = self.get_index(row, col);
+//                 let cell = self.cells[idx];
+//                 next.set(
+//                     idx,
+//                     match (cell, self.live_neighbor_count(row, col)) {
+//                         (true, x) if x < 2 => false,
+//                         (true, 2 | 3) => true,
+//                         (true, x) if x > 3 => false,
+//                         (false, 3) => true,
+//                         (otherwise, _) => otherwise,
+//                     },
+//                 )
+//             }
+//         }
+//
+//         self.cells = next;
 //     }
 // }
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {name}!"));
-}
+//
+// // impl fmt::Display for Universe {
+// //     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+// //         for line in self.cells.as_slice().chunks(self.width as usize) {
+// //             for &cell in line {
+// //                 let symbol = if cell == GameCell::Dead { '◻' } else { '◼' };
+// //                 write!(f, "{symbol}")?;
+// //             }
+// //             write!(f, "\n")?;
+// //         }
+// //
+// //         Ok(())
+// //     }
+// // }
+//
+// #[wasm_bindgen]
+// pub fn greet(name: &str) {
+//     alert(&format!("Hello, {name}!"));
+// }
+//
+// // ----------------------------- Hilbert ------------------------------
+//
+// #[wasm_bindgen]
+// pub fn xy2d(n: u8, x: u8, y: u8) -> u8 {
+//     let halving_iter =
+//         std::iter::successors(Some(n / 2), |&x| if x > 0 { Some(x / 2) } else { None });
+//
+//     for s in halving_iter {
+//         // Your code here...
+//     }
+//     let (rx, ry, s, d) = (false, false, 0, 0);
+//     // for
+//     todo!()
+// }
+//
+// pub fn a() {
+//     std::iter::successors(Some(100), |&x| {
+//         if x > 0 {
+//             println!("{x}");
+//             Some(x / 2)
+//         } else {
+//             None
+//         }
+//     });
+// }
+//
+// pub fn b() {
+//     let mut x = 100;
+//     while x > 0 {
+//         println!("{x}");
+//         x /= 2;
+//     }
+// }
--- www/index.js
+++ www/index.js
@@ -1,86 +1,95 @@
-import { Universe } from "hilbert-curve-wasm";
-import { memory } from "hilbert-curve-wasm/hilbert_curve_wasm_bg.wasm";
+import * as d3 from "d3";
 
-const CELL_SIZE = 5; // px
-const GRID_COLOR = "#CCCCCC";
-const DEAD_COLOR = "#FFFFFF";
-const ALIVE_COLOR = "#000000";
-
-// Construct the universe, and get its width and height.
-const universe = Universe.new();
-const width = universe.width();
-const height = universe.height();
-
-// Give the canvas room for all of our cells and a 1px border
-// around each of them.
-const canvas = document.getElementById("gol-canvas");
-canvas.height = (CELL_SIZE + 1) * height + 1;
-canvas.width = (CELL_SIZE + 1) * width + 1;
-
-const ctx = canvas.getContext("2d");
-
-const renderLoop = () => {
-  universe.tick();
-
-  drawGrid();
-  drawCells();
-
-  requestAnimationFrame(renderLoop);
-};
-
-const drawGrid = () => {
-  ctx.beginPath();
-  ctx.strokeStyle = GRID_COLOR;
-
-  // Vertical lines.
-  for (let i = 0; i <= width; i++) {
-    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
-    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
-  }
-
-  // Horizontal lines.
-  for (let j = 0; j <= height; j++) {
-    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
-    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
-  }
-
-  ctx.stroke();
-};
-
-const getIndex = (row, column) => {
-  return row * width + column;
-};
-
-const bitIsSet = (n, arr) => {
-  const byte = Math.floor(n / 8);
-  const mask = 1 << n % 8;
-  return (arr[byte] & mask) === mask;
-};
-
-const drawCells = () => {
-  const cellsPtr = universe.cells();
-  const cells = new Uint8Array(memory.buffer, cellsPtr, (width * height) / 8);
-
-  ctx.beginPath();
-
-  for (let row = 0; row < height; row++) {
-    for (let col = 0; col < width; col++) {
-      const idx = getIndex(row, col);
-
-      ctx.fillStyle = bitIsSet(idx, cells) ? ALIVE_COLOR : DEAD_COLOR;
-
-      ctx.fillRect(
-        col * (CELL_SIZE + 1) + 1,
-        row * (CELL_SIZE + 1) + 1,
-        CELL_SIZE,
-        CELL_SIZE,
-      );
+function rot(n, x, y, rx, ry) {
+  if (ry === 0) {
+    if (rx === 1) {
+      x[0] = n - 1 - x[0];
+      y[0] = n - 1 - y[0];
     }
+
+    // Swap x and y
+    let t = x[0];
+    x[0] = y[0];
+    y[0] = t;
+  }
+}
+
+function d2xy(n, d, x, y) {
+  let rx, ry;
+  x[0] = y[0] = 0;
+  for (let s = 1; s < n; s *= 2) {
+    rx = 1 & (d / 2);
+    ry = 1 & (d ^ rx);
+    rot(s, x, y, rx, ry);
+    x[0] += s * rx;
+    y[0] += s * ry;
+    d /= 4;
+  }
+}
+
+function drawHilbertCurve(order) {
+  const n = Math.pow(2, order);
+  const maxD = n * n;
+  const points = [];
+
+  for (let d = 0; d < maxD; d++) {
+    let x = [0],
+      y = [0];
+    d2xy(n, d, x, y);
+    points.push([x[0], y[0]]);
   }
 
-  ctx.stroke();
-};
+  const svg = d3
+    .select("#hilbertContainer")
+    .append("svg")
+    .attr("width", 600)
+    .attr("height", 600)
+    .call(
+      d3
+        .zoom()
+        .scaleExtent([1, 40])
+        .wheelDelta((event) => -event.deltaY * 0.001)
+        .on("zoom", (event) => {
+          container.attr("transform", event.transform);
+        }),
+    )
+    .append("g");
 
-drawGrid();
-drawCells();
-requestAnimationFrame(renderLoop);
+  const container = svg.append("g");
+
+  const line = d3
+    .line()
+    .x((d) => d[0] * (600 / n))
+    .y((d) => d[1] * (600 / n));
+
+  container
+    .append("path")
+    .datum(points)
+    .attr("d", line)
+    .attr("stroke", "black")
+    .attr("stroke-width", 0.5)
+    .attr("fill", "none");
+}
+
+drawHilbertCurve(11); // Change the order here
+
+// ----------------------- disable browser's zoom ----------------------
+
+window.addEventListener(
+  "wheel",
+  function (event) {
+    if (event.ctrlKey === true) {
+      event.preventDefault();
+    }
+  },
+  { passive: false },
+);
+
+window.addEventListener("keydown", function (event) {
+  if (
+    event.ctrlKey === true &&
+    (event.key === "+" || event.key === "-" || event.key === "0")
+  ) {
+    event.preventDefault();
+  }
+});
